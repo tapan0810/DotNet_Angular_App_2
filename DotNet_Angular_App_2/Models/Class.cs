b5001027039284150517092e023857472bf7958a
tapan0810 <arceus0810@gmail.com>
2026-08-05 21:27:42 +0530

    feat : Implemented Models

---
+++ DotNet_Angular_App_2/Models/Class.cs
@@ -0,0 +1,10 @@
+﻿namespace DotNet_Angular_App_2.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public char Grade { get; set; } = '\0';
+        public bool IsPassed { get; set; } = false;
+    }
+}
