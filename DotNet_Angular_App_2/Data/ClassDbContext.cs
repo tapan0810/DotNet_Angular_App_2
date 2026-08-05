8809a2d29cdd5d642a4ee650b38afd318de8f04d
tapan0810 <arceus0810@gmail.com>
2026-08-05 21:32:38 +0530

    feat : Implemneted Data ClasDbContext

---
+++ DotNet_Angular_App_2/Data/ClassDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DotNet_Angular_App_2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DotNet_Angular_App_2.Data
+{
+    public class ClassDbContext : DbContext
+    {
+        public ClassDbContext(DbContextOptions<ClassDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Class> Classes => Set<Class>();
+    }
+
+
+}
