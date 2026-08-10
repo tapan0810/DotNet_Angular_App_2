8aed957d88ecd15e14141c1d5a074921a47b1830
tapan0810 <arceus0810@gmail.com>
2026-08-10 22:27:55 +0530

    Fix CORS configuration

--- DotNet_Angular_App_2/Program.cs
+++ DotNet_Angular_App_2/Program.cs
@@ -8,24 +8,58 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
 builder.Services.AddOpenApi();
 
+
+// Database
+
 builder.Services.AddDbContext<ClassDbContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    )
+);
+
+
+// CORS
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AngularPolicy", policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+
+// Repository
 
 builder.Services.AddScoped<IClassRepository, ClassRepository>();
 
+
 var app = builder.Build();
 
+
 // Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
     app.MapScalarApiReference();
 }
 
+
 app.UseHttpsRedirection();
+
+
+// IMPORTANT: CORS must be enabled
+
+app.UseCors("AngularPolicy");
+
 
 app.UseAuthorization();
 
