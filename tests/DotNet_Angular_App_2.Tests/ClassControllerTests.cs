acee13223b9bfc8768c14528c8ef51fe3feafd26
Tapan Ray <arceus0810@gmail.com>
2026-09-08 23:28:16 +0530

    test: fix NUnit compilation failure

--- tests/DotNet_Angular_App_2.Tests/ClassControllerTests.cs
+++ tests/DotNet_Angular_App_2.Tests/ClassControllerTests.cs
@@ -1,3 +1,4 @@
+using System.Threading.Tasks;
 using DotNet_Angular_App_2.Controllers;
 using DotNet_Angular_App_2.Models;
 using DotNet_Angular_App_2.Repositories;
@@ -24,9 +25,7 @@ public class ClassControllerTests
     public async Task GetStudentById_WhenMissing_ReturnsNotFound()
     {
         _repository.Setup(x => x.GetStudentById(99)).ReturnsAsync((Class?)null);
-
         var result = await _controller.GetStudentById(99);
-
         Assert.That(result, Is.TypeOf<NotFoundResult>());
     }
 
@@ -35,9 +34,7 @@ public class ClassControllerTests
     {
         var student = new Class { Id = 1, Name = "Alice", Grade = 'A', IsPassed = true };
         _repository.Setup(x => x.GetStudentById(1)).ReturnsAsync(student);
-
         var result = await _controller.GetStudentById(1);
-
         var ok = result as OkObjectResult;
         Assert.That(ok, Is.Not.Null);
         Assert.That(ok!.Value, Is.SameAs(student));
@@ -48,9 +45,7 @@ public class ClassControllerTests
     {
         var student = new Class { Name = "Alice", Grade = 'A', IsPassed = true };
         _repository.Setup(x => x.AddAsync(student)).Callback(() => student.Id = 7).Returns(Task.CompletedTask);
-
         var result = await _controller.AddStudent(student);
-
         Assert.That(result, Is.TypeOf<CreatedAtActionResult>());
         _repository.Verify(x => x.AddAsync(student), Times.Once);
     }
@@ -59,9 +54,7 @@ public class ClassControllerTests
     public async Task UpdateStudent_WhenIdsDiffer_ReturnsBadRequest()
     {
         var student = new Class { Id = 2, Name = "Alice", Grade = 'A' };
-
         var result = await _controller.UpdateStudent(1, student);
-
         Assert.That(result, Is.TypeOf<BadRequestObjectResult>());
         _repository.Verify(x => x.UpdateAsync(It.IsAny<Class>()), Times.Never);
     }
@@ -71,9 +64,7 @@ public class ClassControllerTests
     {
         var student = new Class { Id = 1, Name = "Alice", Grade = 'A' };
         _repository.Setup(x => x.GetStudentById(1)).ReturnsAsync((Class?)null);
-
         var result = await _controller.UpdateStudent(1, student);
-
         Assert.That(result, Is.TypeOf<NotFoundResult>());
     }
 
@@ -81,9 +72,7 @@ public class ClassControllerTests
     public async Task DeleteStudent_WhenFound_ReturnsNoContent()
     {
         _repository.Setup(x => x.GetStudentById(1)).ReturnsAsync(new Class { Id = 1 });
-
         var result = await _controller.DeleteStudent(1);
-
         Assert.That(result, Is.TypeOf<NoContentResult>());
         _repository.Verify(x => x.DeleteAsync(1), Times.Once);
     }
